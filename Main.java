d77902511e68ac6c7cec9a4c384aa76929991a89
Steve Aronson <saronson@lfschools.net>
2020-08-22 20:04:20 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+/**
+ * Make the letter H as described in the Readme.md
+ * IMPORTANT:  You can only use:
+ *      hop()
+ *      plant()
+ *      turn(LEFT)
+ *  Use left and right arrow keys to see it when running main()
+ */
+public class Main implements Directions {
+
+    /**
+     * Do NOT edit this.  Put your code inside the runJerooCode method below.
+     */
+    public static void main(String[] args) {
+        new JerooGUI();
+        runJerooCode();
+        Map.getInstance().printMap();
+    }
+
+    /**
+     * Put your main Jeroo code here.
+     */
+    public static void runJerooCode() {
+       // Write code here to make a letter here
+       Jeroo kim = new Jeroo(0, 0, EAST, 100);
+    
+    
+    
+
+    }
+
+}
